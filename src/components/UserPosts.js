0666bcbc8ff93554db691141fbe7bc2b406aa7f1
Ihor Oksentiuk <winbka.lem@gmail.com>
2023-09-26 19:02:57 +0300

    Added loader for posts

--- src/components/UserPosts.js
+++ src/components/UserPosts.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { getUserPosts } from "../api/posts";
 import { getPostComments } from "../api/comments";
 import { PostComments } from "./PostComments";
-
+import { Loader } from "./Loader";
 import { useParams } from "react-router-dom";
 import { getUserById } from "../api/users";
 import { Navigation } from "./Navigation";
@@ -14,14 +14,18 @@ export const UserPosts = () => {
   const [userPosts, setUserPosts] = useState([]);
   const [commentsMap, setCommentsMap] = useState({});
   const [showComments, setShowComments] = useState({});
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     if (id) {
+      setLoading(true);
+
       getUserPosts(id)
         .then(setUserPosts)
         .catch(error => {
           console.error('Error while fetching posts:', error);
-        });
+        })
+        .finally(() => setLoading(false));
     }
 
     getUserById(id)
@@ -54,31 +58,37 @@ export const UserPosts = () => {
   return (
     <>
       <Navigation />
-      <h2 className="posts__title">{user.name} posts:</h2>
+      {loading && <Loader />}
 
-      <ul className="posts__container">
-        {
-          userPosts.map(post => (
-            <li
-              key={post.id}
-              className="post"
-            >
-              <p className="post__title">Title: {post.title}</p>
-              <p className="post__body">{post.body}</p>
-              <button
-                className="post__button"
-                onClick={() => toggleComments(post.id)}
-              >
-                {showComments[post.id] ? "Hide comments" : "Show comments"}
-              </button>
+      {!loading && (
+        <>
+          <h2 className="posts__title">{user.name} posts:</h2>
 
-              {showComments[post.id] && (
-                <PostComments comments={commentsMap[post.id] || []} />
-              )}
-            </li>
-          ))
-        }
-      </ul>
+          <ul className="posts__container">
+            {
+              userPosts.map(post => (
+                <li
+                  key={post.id}
+                  className="post"
+                >
+                  <p className="post__title">Title: {post.title}</p>
+                  <p className="post__body">{post.body}</p>
+                  <button
+                    className="post__button"
+                    onClick={() => toggleComments(post.id)}
+                  >
+                    {showComments[post.id] ? "Hide comments" : "Show comments"}
+                  </button>
+
+                  {showComments[post.id] && (
+                    <PostComments comments={commentsMap[post.id] || []} />
+                  )}
+                </li>
+              ))
+            }
+          </ul>
+        </>
+      )}
     </>
   );
 };
